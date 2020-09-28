b72d224164988d59626afcf6860ece21429dda7e
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2020-09-28 10:31:46 +0530

    Add files via upload

---
+++ DataStructures/LinkedList/DoublyLinkedList/DoublyLinkedList_D.java
@@ -0,0 +1,140 @@
+//implemeting deletion od a node from a DLL
+//the element to be deleted may be the head, node other than head or some node which doesn't exist only
+
+import java.util.*;
+
+public class DoublyLinkedList_D
+{
+	Node head;
+	static class Node
+	{
+		int data;
+		Node next;
+		Node prev;
+
+		Node(int d)
+		{
+			data=d;
+		}
+	}
+
+	//pushing node in front
+	public static void push(DoublyLinkedList_D dll,int data)
+	{
+		Node newNode= new Node(data);
+		newNode.next= dll.head;
+		newNode.prev=null;
+		if(dll.head!=null)
+			dll.head.prev=newNode;
+		dll.head=newNode;
+	}
+
+	//insertion after a given node
+
+	public static void insertionAfter(Node prevNode,int data)
+	{
+		if(prevNode==null)
+		{
+			System.out.println("Wrong input");
+			return;
+		}
+		Node newNode= new Node(data);
+		newNode.next=prevNode.next;
+		prevNode.next=newNode;
+		newNode.prev=prevNode;
+		if(newNode.next!=null)
+			newNode.next.prev=newNode;
+	}
+
+	//appending new node at the end
+
+	public static void append(DoublyLinkedList_D dll,int data)
+	{
+		Node newNode= new Node(data);
+		newNode.next=null;
+		Node last= dll.head;
+		if(dll.head==null)
+		{
+			newNode.prev=null;
+			dll.head=newNode;
+			return;
+		}
+		while(last.next!=null)
+			last=last.next;
+		last.next=newNode;
+		newNode.prev=last;
+	}
+
+	//printing
+
+	public static void printing(Node node)
+	{
+		Node last=null;
+		System.out.println("Your List:");
+		while(node!=null)
+		{
+			System.out.print(node.data+" ");
+			last=node;
+			node=node.next;
+		}
+		System.out.println();
+	}
+
+	//deletion
+
+	public static void deletion(DoublyLinkedList_D dll,Node del)
+	{
+		//base case
+		if(dll.head==null || del==null)
+			return;
+		//if the node to be deleted is head only
+		if(del==dll.head)
+			dll.head=del.next;
+		//if there exists a node after the node to be deleted
+		if(del.next!=null)
+			del.next.prev=del.prev;/*the prev of the node next to the node to be deleted must be the prev of del */
+		//if there exists a node previous to the node to be deleted
+		if(del.prev!=null)
+			del.prev.next=del.next;/*the next of the node previous to the node to be deleted must be the next of del*/
+		return;
+	}
+
+	//main method
+
+	public static void main(String args[])
+	{
+		DoublyLinkedList_D dll= new DoublyLinkedList_D();
+		push(dll,1);
+		append(dll,2);
+		push(dll,0);
+		printing( dll.head);
+		append(dll,3);
+		append(dll,4);
+		append(dll,5);
+		append(dll,6);
+		push(dll,-1);
+		printing(dll.head);
+		insertionAfter(dll.head.next.prev.next.prev.next.next,56);
+		printing(dll.head);
+		//deletion
+		deletion(dll,null);
+		System.out.println("No change");
+		printing(dll.head);
+
+		deletion(dll,null);
+		System.out.println("No change");
+		printing(dll.head);
+
+		deletion(dll,dll.head);
+		System.out.println("head deleted");
+		printing(dll.head);
+
+		deletion(dll,dll.head.next.next.next.prev);
+		System.out.println("Changed");
+		printing(dll.head);
+
+		deletion(dll,dll.head);
+		System.out.println("New Head Deleted");
+		printing(dll.head);
+	}
+}
---
+++ DataStructures/LinkedList/DoublyLinkedList/DoublyLinkedList_I.java
@@ -0,0 +1,137 @@
+/*
+Doubly Linked List(DLL)
+Very similar to Singly Linged List, but instead has a pointer pointing to the previous node as well.
+Advantages over Singly Linked List-
+1. we can traverse the linked list, both from forward and backward positions
+2. the deletion and insertion operations become more efficient
+Disadvantages-
+1. extra memory required for the previous pointer
+2. previous pointer have to be maintained also now, for all the operations
+
+//The following shows the insertion of a node in the DLL in four possible ways-
+1. as the first node
+2. as the last node
+3. before a node
+4. after a node
+*/
+
+import java.util.*;
+
+public class DoublyLinkedList_I
+{
+	Node head;
+	static class Node
+	{
+		int data ;
+		Node prev;
+		Node next;
+
+		Node(int d)
+		{
+			data=d;
+		}
+	}
+
+	//method to add a node at the front of the list(as the first node)
+	public static void push(DoublyLinkedList_I dll,int data)
+	{
+		Node newHead = new Node(data);//creating the new node
+		newHead.next= dll.head;
+		newHead.prev= null;
+
+		if(dll.head!=null)
+			dll.head.prev=newHead;
+
+		dll.head=newHead;
+	}
+
+	//method to add a node after a given node
+	public static void insertionAfter(Node prevNode,int data)
+	{
+		//doing check of a situation which is not favourable
+		if(prevNode==null)
+		{
+			System.out.println("Wrong input");
+			return;
+		}
+		//creating a new node with given data
+		Node afterNode=new Node(data);
+		/*the next of our new node must be the next of the node given to us(after which new node had to be inserted)*/
+		afterNode.next=prevNode.next;
+		/*the next of the given node must be our new node*/
+		prevNode.next=afterNode;
+		/*the prev of our new node must be node given to us*/
+		afterNode.prev=prevNode;
+		if(afterNode.next!=null/*if our new node is not the last node in the list*/)
+			afterNode.next.prev=afterNode;/*the node next to our new node, must have it's prev as our new node*/
+	}
+
+	//method to add a node at the end
+
+	public static void append(DoublyLinkedList_I dll,int data)
+	{
+		Node newNode=new Node(data);
+		Node last= dll.head;//to traverse the DLL, till the last node
+		newNode.next=null;//this is our latest element
+		//if the list is empty
+		if(dll.head==null)
+		{
+			newNode.prev=null;
+			dll.head=newNode;
+			return;
+		}
+		//else
+
+		while(last.next!=null)
+			last=last.next;
+		//the next of the last node must be our new node
+		last.next=newNode;
+		//the prev of our new node must be the previous last node
+		newNode.prev=last;
+	}
+
+	//method to print the contents of our DLL, starting from a given node
+
+	public static void printing(Node node)
+	{
+		Node last=null;
+		System.out.println("Traversal in forward direction-");
+		while(node!=null)
+		{
+			System.out.print(node.data+" ");
+			last=node;
+			node=node.next;
+		}
+		System.out.println("\nTraversal in backward direction-");
+		while(last!=null)
+		{
+			System.out.print(last.data+" ");
+			last=last.prev;
+		}
+	}
+
+	//main method
+	public static void main(String args[])
+	{
+		DoublyLinkedList_I dll= new DoublyLinkedList_I();
+		push(dll,1);
+		append(dll,2);
+		push(dll,0);
+		System.out.println("First insertion:");
+		printing(dll.head);
+		push(dll,-1);
+		append(dll,3);
+		append(dll,4);
+		append(dll,5);
+		append(dll,6);
+		System.out.println("\nChanged:");
+		printing(dll.head);
+		insertionAfter(dll.head.next,8);
+		System.out.println("\nChanged:");
+		printing(dll.head);
+		insertionAfter(dll.head.next.next.next.prev.next,9);
+		System.out.println("\nChanged:");
+		printing(dll.head);
+
+	}
+}
