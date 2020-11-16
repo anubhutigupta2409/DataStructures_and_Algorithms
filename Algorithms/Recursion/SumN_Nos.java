fb74c21309e68e5e6fd35aadc70063ec20f6cee1
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2020-11-16 16:05:38 +0530

    Add files via upload

---
+++ Algorithms/Recursion/SumN_Nos.java
@@ -0,0 +1,28 @@
+/*
+find the sum of n nos. using recursion
+*/
+
+import java.util.*;
+
+public class SumN_Nos
+{
+	static int sum(int n)
+	{
+		//base case
+		if(n==1)
+			return 1;
+		//generalise
+		else
+			return n+ sum(n-1);//sum(5)=sum(4)+1 <-- Example
+
+		//you can ask that how we came to knw the value of sum(4)--> but we say ,assume we knw its value-->that's called
+		//recursive leap of faith
+	}
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value of n");
+		int n=sc.nextInt();
+		System.out.println("Sum= "+sum(n));
+	}
+}
