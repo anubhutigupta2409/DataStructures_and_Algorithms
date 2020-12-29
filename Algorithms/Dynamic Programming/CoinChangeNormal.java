4775b54929645a30367bdeca3325a22b09ae5ff0
anubhuti199301171 <56643076+anubhuti199301171@users.noreply.github.com>
2020-12-29 11:50:55 +0530

    Update CoinChangeNormal.java

--- Algorithms/Dynamic Programming/CoinChangeNormal.java
+++ Algorithms/Dynamic Programming/CoinChangeNormal.java
@@ -10,8 +10,8 @@
 //here's how to develop the above approach in the code
 /*
 
-			18 {1,5,7}
-			  /   |   \
+	    18 {1,5,7}
+              /   |   \
           (1)/ (5)| (7)\		<---(used coin in brackets)
             /     |     \
            17     13     11		<---(subtracting to get the left money)
